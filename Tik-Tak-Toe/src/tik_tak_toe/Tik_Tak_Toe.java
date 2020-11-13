cc4868ce8a44b17dfab2ad15be7a9fe7da079baa
Passi <Passi@DESKTOP-4JHAD1C>
2020-11-13 09:43:36 +0100

    Fehlerbehebung

--- Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
+++ Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
@@ -3,15 +3,15 @@ package tik_tak_toe;
 public class Tik_Tak_Toe {
 
 	public static void main(String[] args) {
-		boolean gameIsDone, playing
-		int[] boardData //Das ist ein Array, bei welcher Stelle X oder O oder leer ist
+		boolean gameIsDone, playing;
+		char[] boardData; //Das ist ein Array, bei welcher Stelle X oder O oder leer ist
 		for (int i = 0; i < 9; i++) { //Diese Loop fügt bei den boardData erst die leer-Zeichen
 			boardData[i] = " ";
 			System.out.println(boardData[i]);
 		}
-		playing = true //Zeigt den Status, ob man spielen will für eine while-Schleife (Falls der Kunde nicht mehr spielen will
+		playing = true; //Zeigt den Status, ob man spielen will für eine while-Schleife (Falls der Kunde nicht mehr spielen will
 		while(playing) { //Spiel beginnt
-			gameIsDone = false //Zeigt Status, ob das Spiel fertig ist
+			gameIsDone = false; //Zeigt Status, ob das Spiel fertig ist
 			while(gameIsDone == false) { //Schleife, bis das Spiel fertig ist
 				
 			}
