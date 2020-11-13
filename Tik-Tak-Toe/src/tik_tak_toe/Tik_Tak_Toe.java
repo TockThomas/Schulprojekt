ee0483d3881c819b1b2a8d2d0a6fb17dafaa9246
TockThomas <thomas.schleicher@fastad.de>
2020-11-13 08:52:29 +0100

    Keine Ahnung, ob das funktioniert, weil es bei mir nicht ausführen will

--- Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
+++ Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
@@ -3,7 +3,21 @@ package tik_tak_toe;
 public class Tik_Tak_Toe {
 
 	public static void main(String[] args) {
-		System.out.println("Test");
+		boolean gameIsDone, playing
+		int[] boardData //Das ist ein Array, bei welcher Stelle X oder O oder leer ist
+		for (int i = 0; i < 9; i++) { //Diese Loop fügt bei den boardData erst die leer-Zeichen
+			boardData[i] = " ";
+			System.out.println(boardData[i]);
+		}
+		playing = true //Zeigt den Status, ob man spielen will für eine while-Schleife (Falls der Kunde nicht mehr spielen will
+		while(playing) { //Spiel beginnt
+			gameIsDone = false //Zeigt Status, ob das Spiel fertig ist
+			while(gameIsDone == false) { //Schleife, bis das Spiel fertig ist
+				
+			}
+		}
 	}
-
+	public static void showBoard(boardData) { //Funktion, um den Board darzustellen
+		
+	}
 }
