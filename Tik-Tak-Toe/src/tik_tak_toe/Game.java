4e2c4766f1492c441e15ffdbe7cc510b031c972a
TockThomas <thomas.schleicher@fastad.de>
2020-11-14 14:45:36 +0100

    Erweiterung des Spieles

--- Tik-Tak-Toe/src/tik_tak_toe/Game.java
+++ Tik-Tak-Toe/src/tik_tak_toe/Game.java
@@ -1,18 +1,19 @@
 package tik_tak_toe;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Game {
-	String playerLetter, computerLetter;
+	String playerLetter, computerLetter, turn;
+	Scanner scanner = new Scanner(System.in);
 	
 	public Game() {
 		this.playerLetter = "";
 		this.computerLetter = "";
+		this.turn = "";
 	}
 	
 	public String playerLetter() {	//Methode, damit der Player sich zwischen X oder O entscheidet
-		String playerLetter = "";
-		Scanner scanner = new Scanner(System.in);
 		System.out.println("\nMöchten Sie 'X' oder 'O' sein?");
 		while (true) {
 			playerLetter = scanner.next();
@@ -24,10 +25,41 @@ public class Game {
 				playerLetter = "O";
 				break;
 			} else {
-				System.out.println("Geben Sie eine gültige Antwort.");
+				System.out.println("Geben Sie eine gültige Antwort ein.");
 			}
 		}
+
 		return playerLetter;
+	}
+	
+	public String computerLetter(){
+		if (playerLetter == "X") {
+			computerLetter = "O";
+			return computerLetter;
+		} else {
+			computerLetter = "X";
+			return computerLetter;
+		}
+	}
+	
+	public String whoBegins() {
+		int randomNum = ThreadLocalRandom.current().nextInt(0, 1 + 1);
+//		System.out.println(randomNum);
+		if (randomNum == 1) {
+			turn = playerLetter;
+		} else {
+			turn = computerLetter;
+		}
+		return turn;
+	}
+	
+	public String changeTurn() {
+		if (turn == "X") {
+			turn = "O";
+		} else {
+			turn = "X";
+		}
+		return turn;
 	}
 
 }
--- Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
+++ Tik-Tak-Toe/src/tik_tak_toe/Tik_Tak_Toe.java
@@ -6,22 +6,28 @@ public class Tik_Tak_Toe {
 		Board b1 = new Board();		//Hier mit erstelle ich ein neues Objekt (glaub ich)
 		Game game = new Game();
 		boolean playing = true, gameIsDone;
-		String player, computer;
+		String player, computer, turn;
+		System.out.println("Willkommen bei Tik-Tak-Toe!");
 		
 		while (playing) {
 			player = "";
 			computer = "";
-			System.out.println("Willkommen bei Tik-Tak-Toe!");
-			player = game.playerLetter(); //Der Player entscheidet X oder O
-			//Methode, damit der Computer sich für das Gegenteil entscheidet
-			//Methode, wer zufällig anfängt
+			player = game.playerLetter(); //Der Player entscheidet X oder O und speichert es in var player
+			computer = game.computerLetter(); //Trägt X oder O für KI ein
+			turn = game.whoBegins();//Methode, wer zufällig anfängt
 			gameIsDone = false;
-			while (gameIsDone = false) {
-				//if, wenn Person dran ist
+			while (gameIsDone == false) {
+				if (turn == player){//if, wenn Person dran ist
 					//Befehle folgen noch...
-				//if, wenn computer dran ist
+					turn = game.changeTurn();
+				} else if (turn == computer) {//if, wenn computer dran ist
 					//Befehle folgen noch...
-				//if, Spiel zu ende, wird "gameIsDone" auf true gelegt.
+					turn = game.changeTurn();
+				}
+//				if (Hier sollte dein Überprüfen Methode sein) {//if, Spiel zu ende, wird "gameIsDone" auf true gelegt.
+//					gameIsDone = true;					
+//				}
+				System.out.println(turn);
 			}
 			//Methode, um nachzufragen, ob man nochmal spielen will (ändert den var "playing"
 		}
